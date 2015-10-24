15e5c81ce15566cb6a475a36d1250f6089252040
Matt McNally <mgmcnally@umass.edu>
2015-10-24 11:43:45 -0400

    more comments

--- body.js
+++ body.js
@@ -2,7 +2,11 @@
 //var D = Math.pow(10,28);
 var D = 1;
 
-// name: string, mass: int, pos: array of 2 ints, vel: array of 2 ints, color: rgb color
+// name: string, 
+// mass: int
+// pos: array of 2 ints
+// vel: array of 2 ints
+// color: rgb color
 var Body = function(name, mass, pos, vel, color){
   radius = mass/D;
   return {'name': name, 'mass': mass, 'radius': radius, 'pos': pos, 'vel': vel, 'color' : color};
