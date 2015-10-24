fc7c6e2400ad3962b5ff34bf51a91e877125e685
Carter Tiernan <ctiernan@umass.edu>
2015-10-24 11:39:59 -0400

    paired down version to begin force calculations

--- body.js
+++ body.js
@@ -2,7 +2,7 @@
 //var D = Math.pow(10,28);
 var D = 1;
 
-var Body = function(name, mass, pos, vel){
+var Body = function(name, mass, pos, vel, color){
   radius = mass/D;
-  return {'name': name, 'mass': mass, 'radius': radius, 'pos': pos, 'vel': vel};
+  return {'name': name, 'mass': mass, 'radius': radius, 'pos': pos, 'vel': vel, 'color' : color};
 }
