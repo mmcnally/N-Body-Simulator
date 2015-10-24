099c45a4ba0d23ba3ed7f3149946f601c1330318
Carter Tiernan <ctiernan@umass.edu>
2015-10-24 19:08:26 -0400

    did some stuff, hate me for the terrible commit message

--- simulator.py
+++ simulator.py
@@ -109,62 +109,9 @@ def update_bodies():
 
 def build_bodies():
     add_body('Sun', 1988.92, 100, 0, 0, 0, 0, 'rgba(255, 204, 0, 1.0)')
-    # sun = Body()
-    # sun.name = 'Sun'
-    # # sun.mass = 1.98892 * 10**30
-    # # sun.size = sun.mass / (10 ** 28)
-    # sun.mass = 1988.92
-    # sun.size = 100
-    # sun.px = 0
-    # sun.py = 0
-    # sun.vx = 0
-    # sun.vy = 0
-    # sun.color = 'rgba(255, 204, 0, 1.0)'
+    add_body('Earth', 5.9742, 5, -220, 0, 0, 3, 'rgba(98,100,255, 1.0)')
+    add_body('Venus', 10.8685, 7, 300, 0, 0, 2.5, 'rgba(140, 98, 2, 1.0)')
 
-    add_body('Earth', 5.9742, 5, -325, 0, 0, 1, 'rgba(140, 98, 2, 1.0)')
-
-    # earth = Body()
-    # earth.name = 'Earth'
-    # # earth.mass = 5.9742 * 10**24
-    # # earth.size = earth.mass / (10 ** 28)
-    # earth.mass = 5.9742
-    # earth.size = 5
-    # # earth.px = -1*AU
-    # earth.px = -200
-    # earth.py = 0
-    # earth.vx = 1
-    # earth.vy = 2
-    # earth.color = 'rgba(113, 170, 255, 1.0)'
-
-    add_body('Venus', 10.8685, 7, 300, 0, 0, 1, 'rgba(140, 98, 2, 1.0)')
-
-    # # Venus parameters taken from
-    # # http://nssdc.gsfc.nasa.gov/planetary/factsheet/venusfact.html
-    # venus = Body()
-    # venus.name = 'Venus'
-    # venus.mass = 10.8685
-    # venus.size = 7
-    # venus.px = -325
-    # venus.py = 0
-    # venus.vx = 0
-    # venus.vy = 1
-    # venus.color = 'rgba(140, 98, 2, 1.0)'
-
-    add_body('moon', .02, 1, 100, 100, 0, 0, 'rgba(140, 98, 2, 1.0)')
-
-    # venus = Body()
-    # venus.name = 'moon'
-    # venus.mass = .02
-    # venus.size = 1
-    # venus.px = 100
-    # venus.py = 100
-    # venus.vx = 0
-    # venus.vy = 0
-    # venus.color = 'rgba(140, 98, 2, 1.0)'
-
-    # all_bodies.append(sun)
-    # all_bodies.append(earth)
-    # all_bodies.append(venus)
 
 def add_body(name, mass, size, px, py, vx, vy, color):
     bod = Body()
